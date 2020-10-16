2f877d05438d6b42730a252a9034952a688c78d9
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-10-16 07:49:19 -0500

    Functions stubbed out and given dummy values for returns for purpose of initial compiling. Constructor and destructor empty for now. Compiling correctly, running with errors as methods not yet properly defined.

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -4,3 +4,60 @@ Coding 05
 Purpose: This program constructs a fully
  functional doubly linked list class.
 **********************/
+
+// include only header file directly
+#include "linkedlist.h"
+
+// all linked list methods go here
+
+/*******************************
+ * constructor and destructor
+*******************************/
+LinkedList::LinkedList() {
+
+}
+LinkedList::~LinkedList() {
+
+}
+
+/*******************************
+ * add, delete, and getNode
+*******************************/
+bool LinkedList::addNode(int, string) {
+    bool ok = false;
+    return ok;
+}
+
+bool LinkedList::deleteNode(int) {
+    bool ok = false;
+    return ok;
+}
+
+bool LinkedList::getNode(int, Data *) {
+    bool ok = false;
+    return ok;
+}
+
+/*******************************
+ * remaining functions
+*******************************/
+void LinkedList::printList(bool) {
+
+}
+
+int LinkedList::getCount() {
+    int count = 0;
+    return count;
+}
+
+bool LinkedList::clearList() {
+    return false;
+}
+
+bool LinkedList::exists(int) {
+    bool exists = false;
+    return exists;
+}
+
+
+
