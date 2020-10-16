fdcf20b4e0eb2d4d504db8483fa951a6095e4529
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-10-16 07:48:06 -0500

    Defined the header file, included data.h, wrote prototypes for all public functions. Placeholder for private functions, none prototyped at this time. Compiling correctly w/ g++.

--- linkedlist.h
+++ linkedlist.h
@@ -4,3 +4,30 @@ Coding 05
 Purpose: This program constructs a fully
  functional doubly linked list class.
 **********************/
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include "data.h"
+
+class LinkedList {
+
+public:
+    LinkedList(); // constructor
+    ~LinkedList(); // destructor
+
+    bool addNode(int, string);
+    bool deleteNode(int);
+    bool getNode(int, Data*);
+    void printList(bool=false);
+    int getCount();
+    bool clearList();
+    bool exists(int);
+
+private:
+    // private vars or method prototypes go here
+    // none for now but may create helper functions later
+
+}; // end class
+
+#endif //LINKED_LIST_H
