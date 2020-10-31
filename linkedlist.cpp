a4bb8cfeb23d41918e791ee5a0ad7820d078c039
Allison Elizabeth <allisonelizabeth@Allisons-MacBook-Air.local>
2020-10-31 12:19:23 -0500

    Delted some extra comments and combined some lines to shorten addNode where possible. Added stub for addHeadNode and added call to addHeadNode inside of addNode where conditions warrant the passed Data struct to replace the head.

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -23,39 +23,41 @@ LinkedList::~LinkedList() {
 /*******************************
  * add, delete, and getNode
 *******************************/
-bool LinkedList::addNode(int passedID, string passedData) {
+bool LinkedList::addNode(int passedID, string passedInfo) {
     bool ok = false;
-    Node *thisNode;
+    Node *current;
+    Node *previous;
 
     // check if given id is negative or already exists
-    if (passedID < 0) {
-        ok = false;
-    }
-    else if (head == NULL) {
-        // if head is null, assign passed args to head
-        // prev and next will be NULL
+    if (passedID < 0) {ok = false;}
+    else if (head == NULL) { // if head null, assign passed args to head
         ok = true;
         head = new Node;
         head->data.id = passedID;
-        head->data.data = passedData;
+        head->data.data = passedInfo;
         head->prev = NULL;
         head->next = NULL;
     }
-    else { // if not negative and not first item (head) of list, then...
-        /* compare to first node
-         *   if not a dupe, check < and >
-         *      if less than head, use addHead function
-         *      if greater than head, go to next
-         * at next, can create a loop for non-head nodes
-         * at next, if not dupe, check < and > current
-         *      if less than current, insert before current
-         *      else (greater than current),
-         *          check if next node to see if adding middle or tail */
-
-        // if head exists, compare passedID to ID of next node in list
-    }
-
+    else { // if not negative and head exists
+        current = head;
+        while (current) { //move to next, compare passedID to ID in that node
+            if (passedID == current->data.id) { ok = false; }
+            else {
+                ok = true;
+                // if added node needs to replace the head node
+                if (current->data.id == head->data.id &&
+                passedID < current->data.id) {
+                    // call method for adding head node
+                    addHeadNode(passedID, passedInfo);
+                } // end if
+            } // end else
+        } // end while
+    } // end outer else (for valid data that is not the first node added)
     return ok;
+}
+
+void LinkedList::addHeadNode(int, string) {
+    
 }
 
 bool LinkedList::deleteNode(int) {
