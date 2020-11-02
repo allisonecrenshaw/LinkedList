b34cc6e2af1106c6e7ee01d0c5306182ebe5d76c
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-01 22:48:51 -0600

    Fixed logical errors with flow to select adding head, middle, or tail. Fixed logic within addMiddleNode so that endless while loop doesn't occur (incorrect order when reassigning prev and next). Code is compiling and running properly when adding nodes that are unique but for some reason, if-else statement is not properly avoiding adding duplicate nodes.

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -14,7 +14,7 @@ Purpose: This program constructs a fully
  * constructor and destructor
 *******************************/
 LinkedList::LinkedList() {
-    head = NULL;
+    head = nullptr;
 }
 LinkedList::~LinkedList() {
 
@@ -31,19 +31,24 @@ LinkedList::~LinkedList() {
 bool LinkedList::addNode(int passedID, string passedString) {
     bool ok = false;
 
-    // check if given id is negative or already exists
-    if (passedID < 0) {ok = false;}
-    else if (head == NULL) { // if head null, assign passed args to head
-        ok = true;
-        head = new Node;
-        head->data.id = passedID;
-        head->data.data = passedString;
-        head->prev = NULL;
-        head->next = NULL;
+    // check if given id is negative
+    if (passedID > 0) {
+        if (head == nullptr) { // if head null, assign passed args to head
+            ok = true;
+            head = new Node;
+            head->data.id = passedID;
+            head->data.data = passedString;
+            head->prev = nullptr;
+            head->next = nullptr;
+            cout << "Original head node added." << endl;
+        }
+        else { // if not negative and head exists
+            ok = compareNodes(passedID, passedString);
+        } // end outer else (for valid data that is not the first node added)
+    } // end of if value is non-negative
+    else {
+        cout << "Given value is negative." << endl;
     }
-    else { // if not negative and head exists
-        ok = compareNodes(passedID, passedString);
-    } // end outer else (for valid data that is not the first node added)
     return ok;
 } // end addNode
 
@@ -56,21 +61,40 @@ bool LinkedList::compareNodes(int passedID, string passedString) {
 
     // start with head and begin comparisons
     current = head;
-    while (current && passedID != current->data.id && ok == false) {
-        if (passedID < current->data.id) { // if less than current node, insert before
-            if (!current->prev) // if needing to replace head node
+
+    // loop as long as something exists and ok is false
+    while (current && !ok) {
+        if (passedID == current->data.id) {
+            cout << "No-go, this is a duplicate." << endl;
+            current = nullptr; // break the loop if duplicate
+        }
+        else { // if it's not a duplicate
+            // if passed val is less than the current head
+            if (passedID < current->data.id && !current->prev) { // aka if < head
+                cout << "Adding a new head node." << endl;
                 addHeadNode(passedID, passedString);
-            else if (!current->next)
-                addTailNode(passedID, passedString);
-            else
-                addMiddleNode(passedID, passedString, previous, current);
-            ok = true;
-        } // end if clause for inserting before current node
-        else {
-            previous = current;
-            current = current->next; // move to next node and repeat loop
-        }// already checked for dupe, already checked for insertion
-    } // end while
+                ok = true;
+            } // end addHeadNode clause
+            else if (passedID > current->data.id && !current->next) {
+                // if greater than current and current is tail
+                cout << "Adding a new tail node." << endl;
+                addTailNode(passedID, passedString, current);
+                ok = true;
+            } // end addTailNode clause
+            else if (passedID < current->data.id) {
+                // less than current, but current isn't the head
+                cout << "Adding a middle node." << endl;
+                addMiddleNode(passedID, passedString, current, previous);
+                cout << "Changing ok to true." << endl;
+                ok = true;
+            }
+            else {
+                previous = current;
+                current = current->next;
+            }
+        } // end of else (not a duplicate)
+    } // end while loop
+
     return ok;
 } // end compareNodes
 
@@ -88,10 +112,11 @@ void LinkedList::addHeadNode(int passedID, string passedString) {
     newNode->data.id = passedID;
     newNode->data.data = passedString;
     newNode->next = head;
+    newNode->prev = nullptr;
 
-    // now put in head's position and reassign things as needed
-    head->prev = newNode;
-    head = newNode;
+    // now put new Node as head's value and reassign prev/next as needed
+    head->prev = newNode; // moves current head up in list
+    head = newNode; // gives new value to head
 } // end addHeadNode()
 
 void LinkedList::addMiddleNode(int passedID, string passedString,
@@ -102,19 +127,22 @@ void LinkedList::addMiddleNode(int passedID, string passedString,
     newNode->data.data = passedString;
 
     // reassign previous and next vals so that links are not lost
-    current->prev = newNode;
     previous->next = newNode;
     newNode->prev = previous;
+    current->prev = newNode;
     newNode->next = current;
 } // end addMiddleNode()
 
-void LinkedList::addTailNode(int passedID, string passedString) {
+void LinkedList::addTailNode(int passedID, string passedString, Node* current) {
     // allocate mem for new node
     Node *newNode = new Node;
     newNode->data.id = passedID;
     newNode->data.data = passedString;
 
-    // reassign 
+    // reassign current to point to new tail, make tail point to current as its prev
+    newNode->prev = current;
+    newNode->next = nullptr;
+    current->next = newNode;
 } // end addTailNode()
 
 
