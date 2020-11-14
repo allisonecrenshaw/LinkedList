985e41a438f453ea30ebe84704d0dc1926951238
Allison Elizabeth <allisonelizabeth@Allisons-Air.lan1>
2020-11-14 15:29:23 -0600

    Endless while loop fixed by swapping previous and next args in call to addMiddleNode. Still seeing segmentation fault when looping through list and reaching tail.

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -14,7 +14,7 @@ Purpose: This program constructs a fully
  * constructor and destructor
 *******************************/
 LinkedList::LinkedList() {
-    head = nullptr;
+    head = NULL;
 }
 LinkedList::~LinkedList() {
 
@@ -33,18 +33,21 @@ bool LinkedList::addNode(int passedID, string passedString) {
 
     // check if given id is negative
     if (passedID > 0) {
-        if (head == nullptr) { // if head null, assign passed args to head
+        if (head == NULL) { // if head null, assign passed args to head
             ok = true;
             head = new Node;
             head->data.id = passedID;
             head->data.data = passedString;
-            head->prev = nullptr;
-            head->next = nullptr;
+            head->prev = NULL;
+            head->next = NULL;
+            cout << "First head added...";
         }
         else { // if not negative and head exists
             ok = compareNodes(passedID, passedString);
         } // end outer else (for valid data that is not the first node added)
     } // end of if value is non-negative
+
+    printList();
 
     return ok;
 } // end addNode
@@ -56,17 +59,25 @@ bool LinkedList::compareNodes(int passedID, string passedString) {
     bool stop = false;
     Node *current;
     Node *previous;
+    int loopCount = 0;
 
     // start with head
     current = head;
 
     // while not ok and next still exists
-    while (current && !stop) {
-        // if possible insertion before current, stop.
-        if (passedID < current->data.id)
+    cout << "Begin while loop." << endl;
+    while (current && stop == false && loopCount < 20) {
+        loopCount++;
+        cout << "Loop #" << loopCount << "...current ID is " << current->data.id << endl;
+        if (passedID < current->data.id) {
+            cout << "Passed ID < current ID, changing stop to true." << endl;
             stop = true;
+        }
         else {
+            cout << "Moving to next item." << endl;
+            cout << "Changing previous to current"<< endl;
             previous = current;
+            cout << "Changing current to next." << endl;
             current = current->next;
         }
     } // end while
@@ -75,14 +86,17 @@ bool LinkedList::compareNodes(int passedID, string passedString) {
     if (passedID < current->data.id && !current->prev) {
         ok = true;
         addHeadNode(passedID, passedString);
+        cout << "Head added...";
     }
     else if (passedID < current->data.id && passedID != previous->data.id) {
         ok = true;
-        addMiddleNode(passedID, passedString, current, previous);
+        addMiddleNode(passedID, passedString, previous, current);
+        cout << "Middle added...";
     }
-    else if (passedID > current->data.id && !current->next) {
+    else if (passedID > current->data.id) {
         ok = true;
         addTailNode(passedID, passedString, current);
+        cout << "Tail added...";
     }
 
     return ok;
@@ -101,10 +115,10 @@ void LinkedList::addHeadNode(int passedID, string passedString) {
     Node *newNode = new Node; // will hold passed data
     newNode->data.id = passedID;
     newNode->data.data = passedString;
-    newNode->next = head;
-    newNode->prev = nullptr;
 
-    // now put new Node as head's value and reassign prev/next as needed
+    // reassign next and prev as needed
+    newNode->next = head;
+    newNode->prev = NULL;
     head->prev = newNode; // moves current head up in list
     head = newNode; // gives new value to head
 } // end addHeadNode()
@@ -124,14 +138,16 @@ void LinkedList::addMiddleNode(int passedID, string passedString,
 } // end addMiddleNode()
 
 void LinkedList::addTailNode(int passedID, string passedString, Node* current) {
+    cout << "Inside add tail node." << endl;
+    cout << "Current is " << current->data.id << endl;
     // allocate mem for new node
     Node *newNode = new Node;
     newNode->data.id = passedID;
     newNode->data.data = passedString;
 
-    // reassign current to point to new tail, make tail point to current as its prev
+    // reassign current to new tail, make tail point to current as its prev
+    newNode->next = NULL;
     newNode->prev = current;
-    newNode->next = nullptr;
     current->next = newNode;
 } // end addTailNode()
 
@@ -159,6 +175,20 @@ bool LinkedList::getNode(int, Data *) {
  * remaining functions
 *******************************/
 void LinkedList::printList(bool) {
+    Node *current;
+    current = head;
+    string line = "******************************";
+    int loop = 0;
+
+
+    cout << endl << line << endl;
+    cout << "Printing linked list..." << endl;
+    while (current && loop < 20) {
+        loop++;
+        cout << current->data.id << endl;
+        current = current->next;
+    }
+    cout << "End of linked list." << endl << line << endl;
 
 }
 
